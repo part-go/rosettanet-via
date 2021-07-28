40457c69b978ab5607776cff9de77c0129c539d0
bglmmz <lvxiaoyi@juzix.net>
2021-07-28 17:06:50 +0800

    add comments

--- proxy/codec.go
+++ proxy/codec.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"google.golang.org/grpc/encoding"
 	"log"
+	"reflect"
 
 	"github.com/golang/protobuf/proto"
 )
@@ -26,7 +27,11 @@ func CodecWithParent(fallback encoding.Codec) encoding.Codec {
 	return &rawCodec{fallback}
 }
 
-// 自定义原始编码器
+// VIA server收到stream数据后, 入口是 rawCodec.Unmarshal()
+// 首先会尝试用自定义 rawCodec 来反序列化成frame结构，
+// 如果不可以，再调用父编解码器 protoCodec（这个就是 grpc 内部使用的编解码器）来反序列化成响应消息(本地 grpc 服务支持的消息）。
+
+// 自定义编码器，用来反序列号/序列化 frame结构。由于frame结构就是[]byte，因此实际上rawCodec不对数据做任何处理，目的是用来转发数据
 type rawCodec struct {
 	parentCodec encoding.Codec
 }
@@ -39,11 +44,13 @@ type frame struct {
 // 消息转为frame类型，并返回frame.payload中的数据;
 // 如果消息不能转为frame类型，则调用parentCodec的Marshal来序列化
 func (c *rawCodec) Marshal(v interface{}) ([]byte, error) {
-	log.Printf("rawCodec.Marshal: v:%v", v)
 	out, ok := v.(*frame)
 	if !ok {
+		//如果是VIA server服务支持的消息, v就是对应的类型。（如register.RegisterReq消息 和 register.Boolean消息）
 		return c.parentCodec.Marshal(v)
 	}
+	//否则，则认为是需要转发的数据流，把可rawCodec.Unmarshal读出的数据直接返回即可
+	log.Printf("rawCodec.Marshal: just forward stream(out):%v", out.payload)
 	return out.payload, nil
 
 }
@@ -52,11 +59,13 @@ func (c *rawCodec) Marshal(v interface{}) ([]byte, error) {
 // 将消息转为frame类型，并将数据放入frame.payload;
 // 如果消息不能转为frame类型，则调用parentCodec的Unmarshal来反序列化
 func (c *rawCodec) Unmarshal(data []byte, v interface{}) error {
-	log.Printf("rawCodec.Unmarshal: data:%v, v:%v", data, v)
 	dst, ok := v.(*frame)
 	if !ok {
+		//如果是VIA server服务支持的消息, v就是对应的类型。（如register.RegisterReq消息 和 register.Boolean消息）
 		return c.parentCodec.Unmarshal(data, v)
 	}
+	//否则，则认为是需要转发的数据流，读出数据即可
+	log.Printf("rawCodec.Unmarshal: just forward stream(in):%v", data)
 	dst.payload = data
 	return nil
 }
@@ -70,12 +79,12 @@ func (c *rawCodec) Name() string {
 type protoCodec struct{}
 
 func (protoCodec) Marshal(v interface{}) ([]byte, error) {
-	log.Printf("protoCodec.Marshal: v:%v", v)
+	log.Printf("protoCodec.Marshal: v:%v", reflect.TypeOf(v))
 	return proto.Marshal(v.(proto.Message))
 }
 
 func (protoCodec) Unmarshal(data []byte, v interface{}) error {
-	log.Printf("protoCodec.Unmarshal: data:%v, v:%v", data, v)
+	log.Printf("protoCodec.Unmarshal: data:%v, v:%v", data, reflect.TypeOf(v))
 	return proto.Unmarshal(data, v.(proto.Message))
 }
 
